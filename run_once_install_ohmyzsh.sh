d4a483217678c4e08f628d09e67aca624607c0ad
Colin Flanagan <colin.flanagan-morgan@modaoperandi.com>
2026-02-10 14:12:39 -0500

    using oh-my-zsh plugins

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -3,6 +3,9 @@
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Getting ohmyz.sh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 else
     echo ".oh-my-zsh already found, skipping."
 fi
